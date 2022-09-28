5ed554570d427a797626a7ccea567b986a36ff80
Jamie Taber-Plaisted <jamietp113@gmail.com>
2022-09-28 19:08:37 -0400

    Components Part 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -2,13 +2,13 @@ import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 
 const DirectoryScreen = (props) => {
-    const renderDirectoryItem = ({ item: campsite}) => {
+    const RenderDirectoryItem = ({ item: campsite}) => {
         return (
-            <ListItem>
+            <ListItem onPress={() => props.onPress(campsite.id)}>
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>{campsite.descritpion}</ListItem.Subtitle>
+                    <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
                 </ListItem.Content>
             </ListItem>
         )
@@ -17,7 +17,7 @@ const DirectoryScreen = (props) => {
     return (
         <FlatList
             data={props.campsites}
-            renderItem={renderDirectoryItem}
+            renderItem={RenderDirectoryItem}
             keyExtractor={(item) => item.id.toString()}
         />
     )
